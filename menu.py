41c7075a8d6cadf460d52ec1dd1b873c2b775d08
mdshadhin1 <127687568+mdshadhin1@users.noreply.github.com>
2023-04-19 18:17:27 +0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+
+logo = ("""                  \033[1;97m        
+ _____ _____   _   _____ __ __ 
+| __  |_   _|_| |_|__   |  |  |
+|    -| | | |_   _|   __|-   -|
+|__|__| |_|   |_| |_____|__|__|
+""")
+                               
+
+
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.05)
+
+def jala(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+        
+        
+def psb(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.03)
+        
+os.system("clear")
+import requests,os
+print(logo)
+psb('\033[1;97m[\033[92;1m••\033[1;97m]\033[1;33mMIX ID CLONING OLD AND NEW ')
+print('\033[1;97m[\033[92;1m••\033[1;97m]\033[1;93mPASSWORD CUSTOMISE SYSTEM ')
+jala('\033[1;97m[\033[92;1mヅ\033[1;97m]ZX×RIFAT')
+print('')
+zx = input('\033[1;97m[\033[92;1m 🍪 \033[1;97m]\033[1;92mDo you want to show\033[1;96m cookies\033[1;91m? \033[1;92m[y/n]:')
+os.system("clear")
+import requests,os
+print(logo)
+print('\033[1;97m[\033[92;1m••\033[1;97m]\033[1;33mMIX ID CLONING OLD AND NEW ')
+print('\033[1;97m[\033[92;1m••\033[1;97m]\033[1;93mPASSWORD CUSTOMISE SYSTEM ')
+print('\033[1;97m[\033[92;1mヅ\033[1;97m]ZX×RIFAT')
+print('')
+jalan('\033[1;97m[\033[92;1m『』\033[1;97m]\033[1;97mLOADING\033[1;32m▂ \033[1;32m▄ \033[1;32m▅ \033[1;32m▆ \033[1;32m▇ \033[1;92m█ \033[1;37m║》\033[1;32m✔')
+time.sleep(0.6)
+
+if zx in ['n','y']:
+   os.system("cd more")
+   os.system("python men.py")
